5a822ed3fe8c3723bcc516850b07dbebe287be51
waveOutReset <ichikawaorkouki@gmail.com>
2016-12-18 00:29:29 +0900

    first commit

---
+++ Digispark-SNES-Controller.ino
@@ -0,0 +1,56 @@
+#include "DigiJoystick.h"
+
+#define DAT 0
+#define PS 1
+#define CLK 2
+
+#define PULSE_TIME 3
+
+void pulseClock(byte pin) {
+  digitalWrite(pin, HIGH);
+  delayMicroseconds(PULSE_TIME);
+  digitalWrite(pin, LOW);
+  delayMicroseconds(PULSE_TIME);
+}
+
+void setup() {
+  pinMode(DAT, INPUT);
+  pinMode(PS, OUTPUT);
+  pinMode(CLK, OUTPUT);
+  digitalWrite(PS, LOW);
+  digitalWrite(CLK, LOW);
+}
+
+void loop() {
+  char buf[8] = {
+    0x80, 0x80, 0x80, 0x80, 0x80, 0x80,
+    0x00, 0x00
+  };
+  pulseClock(CLK);
+  pulseClock(PS);
+  if (!digitalRead(DAT)) buf[6] |= 0x04;
+  pulseClock(CLK);
+  if (!digitalRead(DAT)) buf[6] |= 0x08;
+  pulseClock(CLK);
+  if (!digitalRead(DAT)) buf[6] |= 0x80;
+  pulseClock(CLK);
+  if (!digitalRead(DAT)) buf[6] |= 0x40;
+  pulseClock(CLK);
+  if (!digitalRead(DAT)) buf[1] -= 128;
+  pulseClock(CLK);
+  if (!digitalRead(DAT)) buf[1] += 127;
+  pulseClock(CLK);
+  if (!digitalRead(DAT)) buf[0] -= 128;
+  pulseClock(CLK);
+  if (!digitalRead(DAT)) buf[0] += 127;
+  pulseClock(CLK);
+  if (!digitalRead(DAT)) buf[6] |= 0x02;
+  pulseClock(CLK);
+  if (!digitalRead(DAT)) buf[6] |= 0x01;
+  pulseClock(CLK);
+  if (!digitalRead(DAT)) buf[6] |= 0x10;
+  pulseClock(CLK);
+  if (!digitalRead(DAT)) buf[6] |= 0x20;
+  DigiJoystick.setValues(buf);
+  DigiJoystick.update();
+}
